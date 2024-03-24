4779ef6f9fd8fa283d7dd552ba9ebe626f019639
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-03-24 19:35:23 +0530

    feat: add api and all changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Knowledge Cards Frame
+
+Discover and learn new information throught Knowledge Cards.
+
+Developed by Harsh Tyagi
--- app/api/frames/route.jsx
+++ app/api/frames/route.jsx
@@ -45,10 +45,32 @@ const handleRequest = frames(async (ctx) => {
     console.log("results :", results);
     return {
       image: (
-        <div tw="flex flex-col justify-center items-center w-full h-full">
-          <p tw="text-[40px]">Here are your results!</p>
-          <p tw="">{results}</p>
-        </div>
+        <>
+          <div tw="flex flex-col justify-center items-center w-full h-full">
+            {/* <p tw="text-[40px]">Here are your results!</p> */}
+            <p tw="">
+              <span tw="text-[40px]">{results.word}</span> ,{" "}
+              <span tw="font-italic">{results.phonetic}</span>
+            </p>
+            <div tw="flex flex-col items-center gap-0 px-2">
+              <p tw="text-[40px]">Definition : </p>
+              {results.meanings.map((meaning, index) => (
+                <div key={index} tw="flex flex-col">
+                  <p tw="">{meaning.definition}</p>
+                  {/* <p tw="">{meaning.example}</p> */}
+                </div>
+              ))}
+            </div>
+            <div tw="flex flex-row ">
+              <p tw="text-[40px]">Synonyms : </p>
+              {results.synonyms.map((synonym, index) => (
+                <p key={index} tw="">
+                  {synonym} ,
+                </p>
+              ))}
+            </div>
+          </div>
+        </>
       ),
       buttons: [
         <Button action="post" target={{ query: { value: "start" } }}>
@@ -75,6 +97,27 @@ const callApi = async (inputText) => {
   const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputText}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
-  console.log("API results:", data);
-  return inputText;
+  const word = data[0]?.word;
+  const phonetic = data[0]?.phonetic;
+
+  const partOfSpeech = data[0]?.meanings[0]?.partOfSpeech;
+  const meanings = data[0].meanings[0].definitions.slice(
+    0,
+    Math.min(1, data[0].meanings[0].definitions.length)
+  );
+  const synonyms = data[0].meanings[0].synonyms.slice(
+    0,
+    Math.min(2, data[0].meanings[0].synonyms.length)
+  );
+
+  const knowledgeCard = {
+    word: word,
+    phonetic: phonetic,
+    partOfSpeech: partOfSpeech,
+    meanings: meanings,
+    synonyms: synonyms,
+  };
+
+  console.log("API results:", knowledgeCard);
+  return knowledgeCard;
 };
